725aaa31ebd3e910cc11628e2ced3ebfaf9d3311
reskillfrom31-lab <re.skill.from31@gmail.com>
2025-09-15 17:54:20 +0900

    結合テストの実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -18,7 +18,7 @@ class MessagesController < ApplicationController
 
   private
 
-  def messages_params
-    params.require(:message).permit(:content).merge(user_id: current_user.id)
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
   end
 end
